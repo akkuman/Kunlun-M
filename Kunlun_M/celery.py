6c1220ebf21a90b7e795bbcf35a31f84d252936c
悟空 <akkumans@qq.com>
2021-01-14 11:27:26 +0800

    support for accepting tasks as distributed nodes

---
+++ Kunlun_M/celery.py
@@ -0,0 +1,29 @@
+import os
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kunlun_M.settings')
+
+app = Celery('Kunlun_M')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# initialize environment
+import django
+django.setup()
+from core import main
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+# Load task modules from main app
+app.autodiscover_tasks(['Kunlun_M'])
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
---
+++ Kunlun_M/tasks.py
@@ -0,0 +1,63 @@
+from celery import shared_task
+from django.conf import settings
+from tempfile import NamedTemporaryFile
+import shutil
+import os
+import uuid
+from utils.log import logger
+from utils import utils
+from minio.error import S3Error
+from web.index.models import ScanTask, ScanResultTask, Rules, NewEvilFunc
+from core import cli
+from core.engine import Running
+import random
+import string
+
+
+@shared_task
+def kunlun_M_code_audit(source_oss_path: str):
+    """kunlun_M 代码审计
+    
+    Args:
+      - source_path: 源代码路径
+    """
+    target_path = get_source_localpath(source_oss_path)
+    res = ''
+
+    # 开启扫描任务
+    task_name = utils.get_mainstr_from_filename(target_path)
+    s = ScanTask(task_name=task_name, target_path=target_path, parameter_config=[])
+    s.save()
+
+    data = {
+            'status': 'running',
+            'report': ''
+        }
+    Running(s.id).status(data)
+
+    temp_jsonfilepath = f"kunlun_res{''.join(random.sample(string.ascii_letters + string.digits, 8))}.json"
+    cli.start(target_path, 'json', temp_jsonfilepath, None, s.id)
+    with open(temp_jsonfilepath) as ft:
+        res = ft.read()
+    s.is_finished = True
+    s.save()
+
+    # 获取结果
+    return res
+    
+
+def get_source_localpath(source_oss_path: str) -> str:
+    """从oss中获取源码压缩包，解压到本地后返回解压路径"""
+    try:
+        client = settings.MINIO_CLIENT
+        object_suffix = os.path.splitext(source_oss_path)[-1]
+        bucket_name, object_name = source_oss_path.split('/', 1)
+        with NamedTemporaryFile(prefix=bucket_name, dir='/tmp', suffix=object_suffix) as f:
+            client.fget_object(bucket_name, object_name, f.name)
+            source_path = os.path.join(settings.SOURCE_TMP_ROOTPATH, f'sources_{str(uuid.uuid4())}')
+            if not os.path.exists(source_path):
+                os.makedirs(source_path, 0o755)
+            shutil.unpack_archive(f.name, source_path)
+            return source_path
+    except S3Error as e:
+        logger.error("minio occur error: %s", e)
